059a6c92d743ece99ff59a3328c5554bba2c9b6c
Volodymyr Chernyshev <vladimir.chernishov@gmail.com>
2023-05-30 09:34:04 -0500

    Bubble Sort

---
+++ src/main/java/org/ik/sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package org.ik.sort;
+
+import java.util.List;
+
+public class BubbleSort {
+
+    static List<Integer> sort(List<Integer> arr) {
+        if(arr.size() <2) return arr;
+
+        for(int i = 0; i < arr.size() - 1; i++){
+            //bubble from the end.
+            boolean isSwapped = false;
+            for(int j = arr.size() - 1; j > i; j--){
+                if(arr.get(j) <  arr.get(j -1)){
+                    swap(arr, j, j -1);
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped) break;
+        }
+        return arr;
+    }
+
+    static void swap(List<Integer> arr, int a, int b){
+        int value = arr.get(a);
+        arr.set(a, arr.get(b));
+        arr.set(b, value);
+    }
+
+}
