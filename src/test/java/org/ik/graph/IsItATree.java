16af29b9691762e4854991f60b3168fc8698aadf
vlad macbook <vladimir.chernishov@gmail.com>
2023-06-26 11:53:28 -0500

    Graph foundation. IsItATree fixed

--- src/test/java/org/ik/graph/IsItATree.java
+++ src/test/java/org/ik/graph/IsItATree.java
@@ -1,32 +1,33 @@
 package org.ik.graph;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+
+import java.util.*;
 
 public class IsItATree {
 
 
     @Test
     public void test(){
-        System.out.println(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0,0)), new ArrayList<>(Arrays.asList(1,2,3))));
-        System.out.println(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0)), new ArrayList<>(Arrays.asList(1,2))));
-        System.out.println(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0,1,2)), new ArrayList<>(Arrays.asList(3,1,2,0))));
-        System.out.println(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0,0,1)), new ArrayList<>(Arrays.asList(1,2,3,0))));
+        Assertions.assertTrue(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0,0)), new ArrayList<>(Arrays.asList(1,2,3))));
+        Assertions.assertFalse(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0)), new ArrayList<>(Arrays.asList(1,2))));
+        Assertions.assertFalse(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0,1,2)), new ArrayList<>(Arrays.asList(3,1,2,0))));
+        Assertions.assertFalse(is_it_a_tree(4, new ArrayList<>(Arrays.asList(0,0,0,1)), new ArrayList<>(Arrays.asList(1,2,3,0))));
+        Assertions.assertTrue(is_it_a_tree(6, new ArrayList<>(Arrays.asList(4,4,4,2,1)), new ArrayList<>(Arrays.asList(3,5,0,0,0))));
     }
 
     static Boolean is_it_a_tree(Integer n, ArrayList<Integer> edge_start, ArrayList<Integer> edge_end) {
 
         ArrayList<ArrayList<Integer>> adjancencyList = createAdjancencyList(edge_start, edge_end, n);
 
-
         Boolean[] visited = new Boolean[n];
         Arrays.fill(visited, Boolean.FALSE);
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
 
-        if(isBFSHasCycles(adjancencyList, visited, 0)){
+        if(isBFSHasCycles(adjancencyList, visited, 0, parent)){
             return false;
         }
 
@@ -40,18 +41,21 @@ public class IsItATree {
         return true;
     }
 
-    private static boolean isBFSHasCycles(ArrayList<ArrayList<Integer>> adjacencyList, Boolean[] visited, int start) {
+    private static boolean isBFSHasCycles(ArrayList<ArrayList<Integer>> adjacencyList, Boolean[] visited, int start, int[] parent) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
         visited[start] = true;
         while(!queue.isEmpty()){
-            Integer current = queue.poll();
-            for (Integer next : adjacencyList.get(current)) {
-                if(!visited[next]) {
-                    queue.add(next);
-                    visited[next] = true;
+            int current = queue.poll();
+            for (Integer child : adjacencyList.get(current)) {
+                if(!visited[child]) {
+                    parent[child] = current;
+                    queue.add(child);
+                    visited[child] = true;
                 } else {
-                    return true;
+                    if(child != parent[current]){
+                        return true;
+                    }
                 }
             }
         }
@@ -68,7 +72,7 @@ public class IsItATree {
 
         for(int i = 0; i < start.size(); i++){
             ret.get(start.get(i)).add(end.get(i));
-//            ret.get(end.get(i)).add(start.get(i));
+            ret.get(end.get(i)).add(start.get(i));
         }
 
         return ret;
